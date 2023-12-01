3738eb5bb479db154b2f191deed72f91655b0413
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2023-12-01 13:23:13 +0530

    second-commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,44 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  // imports: [
+  //   UsersModule,
+  //   TypeOrmModule.forRoot({
+  //     type: 'postgres',
+  //     host: 'localhost',
+  //     port: 5432,
+  //     username: 'postgres',
+  //     password: 'password',
+  //     database: 'mydb',
+  //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  //     synchronize: true,
+  //   }),
+  // ],
   imports: [
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'mydb',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          // envFilePath: '.local.env',
+          envFilePath: '.prod.env',
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
